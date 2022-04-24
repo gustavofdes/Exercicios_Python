0116fa8466be2e9a1d44bc7b31d3e8e379501c12
GustavoTK <gustavoferreiradoespiritosanto@gmail.com>
2022-04-24 20:41:00 -0300

    atualizacção jogo

--- teste.py
+++ teste.py
@@ -1,31 +1,32 @@
 
+import random
+import time
 
-from time import sleep
-from random import randint
-c = int(input('\033[30mTabuada = 1\nJogo = 2\n->\033[m '))
-if c == 1:
-    print('\033[34mVocê escolheu a Tabuada\033[m')
-    sleep(2)
-    n = int(input('\033[36mDigite um valor:\033[m '))
-    print('\033[33m{} x {} = {}'.format(n, 1, n*1))
-    print('{} x {} = {}'.format(n, 2, n*2))
-    print('{} x {} = {}'.format(n, 3, n*3))
-    print('{} x {} = {}'.format(n, 4, n*4))
-    print('{} x {} = {}'.format(n, 5, n*5))
-    print('{} x {} = {}'.format(n, 6, n*6))
-    print('{} x {} = {}'.format(n, 7, n*7))
-    print('{} x {} = {}'.format(n, 8, n*8))
-    print('{} x {} = {}'.format(n, 9, n*9))
-    print('{} x {} = {}'.format(n, 10, n*10))
-else:
-    print('\033[34mVocê escolheu o Jogo\033[m')
-    sleep(2)
-    print('\033[35mPensei em um número.')
-    sleep(2)
-    e = int(input('\033[36mEscolha um número de 0 a 5:\033[m '))
-    v = randint(0, 5)
-    sleep(2)
-    if e == v:
-        print('\033[32mParabêns! Você ganhou.\033[m')
+print("\033[34m-="*38)
+print("Ola! Seja bem-vindo ao nosso programa! Escolha a opção que deseja usar: ")
+print("-=" * 38 + "\033[m")
+time.sleep(0.5)
+
+print("\033[30mJogo de advinhação --> \033[32m1\033[m")
+print("\033[30mTabuada --> \033[32m2\033[m")
+print("\033[30mVerificador de multa --> \033[32m3\033[m")
+
+opcao = int(input("\033[34mEscolha com a opção desejada: "))
+print("\033[31mAGUARDE...\033[m")
+time.sleep(1)
+
+if opcao == 1:
+    print("")
+    print("A opção selecionada foi \033[4:36mJogo de Adivinhação\033[m !")
+    print("Vou pensar em um numero de 0 a 5 e você tem que acertar qual eu pensei...")
+    time.sleep(0.5)
+    sorteado = random.randint(1, 5)
+    print("Pensando...")
+    time.sleep(2.5)
+    number = int(input("Qual numero você acha que eu pensei? "))
+    if number == sorteado:
+        print(f"\033[32mParabéns!! Eu pensei no numero {sorteado} e você acertou!\033[m")
     else:
-        print('\033[31mVocê perdeu! Eu escolhi {} não {}\033[m'.format(v, e))
+        print(f"\033[31mQue pena... Eu pensei no numero {sorteado} e você errou!\033[m")
+if opcao == 2:
+    print("A opção selecionada foi tabuada")
